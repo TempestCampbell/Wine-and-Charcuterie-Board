9c3502109d6910968213d633dffab98229e796f4
Matthew Kukulski <Matthew.Kukulski@du.edu>
2021-01-29 10:12:05 -0700

    Create Countries Table

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
+from config import pw
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use flask_sqlalchemy to set up sql connection locally
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@server/db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:{pw}@localhost:5432/WineCharcuteriedb'
 db = SQLAlchemy(app)
 
 
