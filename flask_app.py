0c97a17eefe344e90cb03d4983e0c98788ecc867
Matthew Kukulski <Matthew.Kukulski@du.edu>
2021-01-29 20:05:04 -0700

    Database setup in Flask. Test export of sql database.

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 from config import pw
 
+################################################
+# Database Setup
+#################################################
+engine = create_engine("postgres://postgres:{pw}@localhost:5432/WineCharcuterie")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Cheese = Base.classes.cheese
+Countries = Base.classes.countries
+WineCheesePairingData = Base.classes.winecheesepairingdata
+Wines = Base.classes.wines
+Wineries = Base.classes.wineries
+WorldMeats = Base.classes.worldmeats
+
+################################################
+# Flask Setup
+#################################################
 # Create an instance of Flask
 app = Flask(__name__)
 
@@ -9,6 +36,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:{pw}@localhost:5432/WineCharcuterie'
 db = SQLAlchemy(app)
 
+@app.route("/api/v1.0/scatter")
+def scatter():
+
+    # Create our session from Python to the DB
+    session = Session(engine)
 
 
 if __name__ == '__main__':
