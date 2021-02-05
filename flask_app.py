f077496cd8a184976459f7b2a2267657ce94907b
Matthew Kukulski <Matthew.Kukulski@du.edu>
2021-02-04 19:16:09 -0700

    edit updateTable function and dropDown select

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,7 @@ from sqlalchemy import create_engine, func
 # Database Setup
 #################################################
 
-engine = create_engine(f"postgresql://postgres:postgres@localhost:5432/WineAndDined")
+engine = create_engine(f"postgresql://postgres:Deez^nout3@localhost:5432/WineAndDined")
 
 # engine = create_engine("postgresql://postgres:postgres@localhost:5432/WineAndDined")
 
@@ -40,7 +40,7 @@ session=Session(engine)
 app = Flask(__name__)
 
 # Use flask_sqlalchemy to set up sql connection locally
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:postgres@localhost:5432/WineAndDined'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:Deez^nout3@localhost:5432/WineAndDined'
 db = SQLAlchemy(app)
 
 @app.route("/")
--- logic.js
+++ logic.js
@@ -108,6 +108,91 @@ function init() {
     });
 };
 
+var x, i, j, l, ll, selElmnt, a, b, c;
+
+// look for any elements with the class "custom-select":
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+selElmnt = x[i].getElementsByTagName("select")[0];
+ll = selElmnt.length;
+
+// for each element, create a new DIV that will act as the selected item:
+a = document.createElement("DIV");
+a.setAttribute("class", "select-selected");
+a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+x[i].appendChild(a);
+
+// for each element, create a new DIV that will contain the option list:
+b = document.createElement("DIV");
+b.setAttribute("class", "select-items select-hide");
+for (j = 1; j < ll; j++) {
+
+    // for each option in the original select element,
+    // create a new DIV that will act as an option item:
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+
+        // when an item is clicked, update the original select box,
+        // and the selected item:
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+            y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+        }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+}
+x[i].appendChild(b);
+a.addEventListener("click", function(e) {
+
+    // when the select box is clicked, close any other select boxes,
+    // and open/close the current select box:
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+    });
+}
+function closeAllSelect(elmnt) {
+
+// a function that will close all select boxes in the document,
+// except the current select box:
+var x, y, i, xl, yl, arrNo = [];
+x = document.getElementsByClassName("select-items");
+y = document.getElementsByClassName("select-selected");
+xl = x.length;
+yl = y.length;
+for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+    arrNo.push(i)
+    } else {
+    y[i].classList.remove("select-arrow-active");
+    }
+}
+for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+    x[i].classList.add("select-hide");
+    }
+}
+}
+// if the user clicks anywhere outside the select box,
+// then close all select boxes:
+document.addEventListener("click", closeAllSelect);
 // Create map event handler
 // var mapSelect = d3.select("#map");
 // mapSelect.on("click", updateTable);
@@ -116,26 +201,36 @@ function updateTable(countrySelect) {
 
     console.log("here again", countrySelect)
 
-    // dropdown menu to filter table
-    let dropdownBtn = document.querySelector('.menu-btn');
-    let menuContent = document.querySelector('.menu-content');
-    dropdownBtn.addEventListener('click',()=>{
-    if(menuContent.style.display===""){
-        menuContent.style.display="block";
-    } 
-    else {
-        menuContent.style.display="";
-    }
-    });
+    // // dropdown menu to filter table
+    // let dropdownBtn = document.querySelector('.menu-btn');
+    // let menuContent = document.querySelector('#menu-content');
+    // dropdownBtn.addEventListener('click',()=>{
+    // if(menuContent.style.display===""){
+    //     menuContent.style.display="block";
+    // } 
+    // else {
+    //     menuContent.style.display="";
+    // }
+    // });
 
     // Select the input element and get the raw HTML node
     // var inputElement = d3.select(".menu-content");
 
-    // handle on click event
-    d3.select('.menu-content')
-    .on('click', function(dropDownSelect) {
+    // var x = document.getElementById("menu-content");
+    // var i = x.selectedIndex;
+    // var dropDown = x.options[i].text;
 
-        var dropDown = eval(d3.select(this).attr('href'));
+    // handle on click event
+    // d3.select("select")
+    // .on('change', function() {
+
+        var dropDown = d3.select('select').property('value');
+        // var dropDown = this.options[this.selectedIndex].value
+
+        // var sel = document.getElementById("menu-content");
+        // var dropDown= sel.options[sel.selectedIndex].text;
+        // var dropDown = eval(d3.select(this).options[this.selectedIndex].text());
+
         // Get the value property of the input element
         // var dropDown = inputElement.property("value");
         console.log("selection", dropDown)
@@ -179,5 +274,7 @@ function updateTable(countrySelect) {
                 });
             });
         });
-    });
-};
+    };
+
+    d3.select("select")
+    .on('change', updateTable);
