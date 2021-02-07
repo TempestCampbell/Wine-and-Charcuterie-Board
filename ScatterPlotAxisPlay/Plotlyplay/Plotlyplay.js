7826f86612d2d60642a03fa7cd894d8650954cce
Tempest Campbell <TempestCampbell@hotmail.com>
2021-02-06 17:32:51 -0700

    saving changes

--- ScatterPlotAxisPlay/Plotlyplay/Plotlyplay.js
+++ ScatterPlotAxisPlay/Plotlyplay/Plotlyplay.js
@@ -24,68 +24,7 @@ function buildPlot(country) {
         })
         
         console.log(xx)
-//ATTEMPT AT SLIDER ********************************************************
-// //Set up SelectorOptions
-// var selectorOptions = {
-//     buttons: [{
-//         step: 'price',
-//         stepmode: 'todate',
-//         count: 1,
-//         label: '?'
-//     }, {
-//         step: 'price',
-//         stepmode: 'backward',
-//         count: 1,
-//         label: '??'
-//     }, {
-//         step: 'all',
-//     }],
-// };
-// function setBarPlot(chosents) {
-//     // this function sets up the x-y plot for plotly
-//         currentData = getCountryData(chosents);
-//         if (type == 1) { 
-//             var trace1 = {
-//                 x: years,
-//                 y: currentData,
-//                 width: 400,
-//                 height: 500,
-//                 mode: 'lines+markers',
-//                 marker: {
-//                     size: 12,
-//                     opacity: 0.5
-//                 }
-//             }
-//             var layout = {
-//                 title: `Wine`,
-//                 xaxis: { rangeselector: selectorOptions,
-//                 rangeslider: {
-//                 title: "Price($)"
-//                 },
-//                 yaxis: {
-//                     autorange: true,
-//                     title: "Points",
-//                     type: "linear"},
-//             }
-//         else { 
-//             var trace1 = {
-//             marker: {
-//             colors: ["#66FF33","#FF99FF","#FF9900","#CC6600","#FF0000"]},
-//             type: "scatter",
-//             mode: "markers",
-//                 text: hovertext,
-//                 x: xx,
-//                 y: yy,
-//                 line: {
-//                 color: "#380059"
-//             } 
-        
-//     };
-// };
 
-//         var data = [trace1];
-//         Plotly.newPlot("plot", data, layout);//plot data 
-// };
     //ORIGINAL XAXIS***************************************************************************************************8
     var trace1 = {
         type: "scatter",
